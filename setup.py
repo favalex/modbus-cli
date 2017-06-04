fe7ce1f8a29b33ba9b2678e90c1b28a4e3e95b9c
Gabriele Favalessa <favalex@gmail.com>
2017-06-04 12:35:20 +0200

    prepare for distribution

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='modbus-cli',
+      version='0.1.0',
+      description='Command line tool to access Modbus devices',
+      url='http://github.com/favalex/modbus-cli',
+      author='Gabriele Favalessa',
+      author_email='favalex@gmail.com',
+      license='MPL 2.0',
+      scripts=['bin/modbus'],
+      install_requires=[ 'umodbus', ],
+      zip_safe=False,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Embedded Systems',
+      ])
