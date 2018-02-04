bfae0c83562e0f709822f2a0ebcbb502c6700263
Gabriele Favalessa <favalex@gmail.com>
2018-02-04 10:46:43 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup
 
+
 def readme():
     with open('README.rst') as f:
         return f.read()
 
+
 setup(name='modbus_cli',
-      version='0.1.3',
+      version='0.1.4rc0',
       description='Command line tool to access Modbus devices',
       long_description=readme(),
       url='http://github.com/favalex/modbus-cli',
       author='Gabriele Favalessa',
       author_email='favalex@gmail.com',
       license='MPL 2.0',
+      packages=['modbus_cli'],
       scripts=['bin/modbus'],
-      install_requires=[ 'umodbus', ],
+      install_requires=['umodbus'],
       zip_safe=False,
       classifiers=[
           'Development Status :: 4 - Beta',
